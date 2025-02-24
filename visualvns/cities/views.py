b23975e3fd6ef7bb7c142bcaa7bc49283342a4c3
KtSailing <90542082+KtSailing@users.noreply.github.com>
2025-02-24 06:59:22 +0000

    development with IPad is over.

--- visualvns/cities/views.py
+++ visualvns/cities/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from cities.models import City
 
 from cities.forms import CityForm
@@ -21,9 +21,26 @@ def city_new(request):
             city.created_by = request.user
             city.save()
             return redirect(city_detail, city_id=city.pk)
+        
+    else:
+        form = CityForm()
+        return render(request, 'cities/city_new.html', {'form': form})
 
-def city_edit(request):
-    return HttpResponse('都市セットの編集')
+@login_required
+def city_edit(request, city_id):
+    city = get_object_or_404(City, pk=city_id)
+    if city.created_by_id != request.user.id:
+        return HttpResponseForbidden("この都市の編集は許可されていません。")
+    
+    if request.method == 'POST':
+        form = CityForm(request.POST, instance=city)
+        if form.is_valid():
+            form.save()
+            return redirect('city_detail', city_id = city_id)
+    else:
+        form = CityForm(instance=city)
+        return render(request, 'cities/city_edit.html', {'form': form})
+
 
 def city_detail(request, city_id):
     city = get_object_or_404(City, pk=city_id)

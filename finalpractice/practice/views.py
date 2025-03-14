f37458bb3ae08eddab57e2a24a41bf653eb787db
KtSailing <90542082+KtSailing@users.noreply.github.com>
2025-03-14 13:40:15 +0000

    final ipad.

--- finalpractice/practice/views.py
+++ finalpractice/practice/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Route, City, Salesman
+from .forms import CityForm, SalesmanForm, RouteForm
 from .serializers import RouteSerializer, CitySerializer, SalesmanSerializer
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 
 from algorithms.vns_model import model
 
@@ -38,6 +41,74 @@ def top(request):
     routes = Route.objects.prefetch_related("cities").select_related("depot").all()
     context = {"cities": cities, "routes": routes}
     return render(request, "practice/top.html", context)
+
+@login_required
+def city_new(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            city = form.save(commit=False)
+            city.created_by = request.user
+            city.save()
+            return redirect('city_detail', city_id=city.pk)        
+    else:
+        form = CityForm()
+        return render(request, 'practice/city_new.html', {'form': form})
+
+
+@login_required
+def city_edit(request, city_id):
+    city = get_object_or_404(City, pk=city_id)
+    if city.created_by_id != request.user.id:
+        return HttpResponseForbidden("この都市の編集は許可されていません。")
+    
+    if request.method == 'POST':
+        form = CityForm(request.POST, instance=city)
+        if form.is_valid():
+            form.save()
+            return redirect('city_detail', city_id = city_id)
+    else:
+        form = CityForm(instance=city)
+        return render(request, 'practice/city_edit.html', {'form': form})
+
+
+def city_detail(request, city_id):
+    city = get_object_or_404(City, pk=city_id)
+    return render(request, 'practice/city_detail.html', {'city': city})
+
+
+@login_required
+def route_new(request):
+    if request.method == 'POST':
+        form = RouteForm(request.POST)
+        if form.is_valid():
+            route = form.save(commit=False)
+            route.created_by = request.user
+            route.save()
+            form.save_m2m()
+            return redirect('route_detail', route_id=route.pk)
+    else:
+        form = RouteForm()
+        return render(request, "practice/route_new.html", {'form': form})
+
+@login_required
+def route_edit(request, route_id):
+    route = get_object_or_404(Route, pk=route_id)
+    if route.created_by_id != request.user.id:
+        return HttpResponseForbidden("この経路の編集は許可されていません。")
+
+    if request.method == 'POST':
+        form = RouteForm(request.POST, instance=route)
+        if form.is_valid():
+            form.save()
+            return redirect('route_detail', route_id = route_id)
+    else:
+        form = RouteForm(instance=route)
+        return render(request, 'practice/route_edit.html', {'form': form})
+    
+def route_detail(request, route_id):
+    route = get_object_or_404(Route, pk=route_id)
+    return render(request, 'practice/route_detail.html', {'route': route})
 
 def test_detail(request, route_id):
     route = get_object_or_404(Route, uuid=route_id)
